4dcea55f50a020daabde92032c1e8be368a0de3a
modestas612 <43955930+modestas612@users.noreply.github.com>
2018-12-05 15:07:33 +0200

    update uzd 6

--- src/main/java/lt/baltictalents/stoteliutinklas/code/AtstumaiTarpStoteliuPavilijonu.java
+++ src/main/java/lt/baltictalents/stoteliutinklas/code/AtstumaiTarpStoteliuPavilijonu.java
@@ -1,20 +1,38 @@
 package lt.baltictalents.stoteliutinklas.code;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.List;
+
 import lt.baltictalents.stoteliutinklas.data.beans.Station;
 import lt.baltictalents.stoteliutinklas.data.hardcode.HardCodedDb;
 
 public class AtstumaiTarpStoteliuPavilijonu {
 	
-	private String atstumasMetrais;
+	private double atstumasMetrais;
 	private HardCodedDb stoteles = new HardCodedDb();
 	
-	public AtstumaiTarpStoteliuPavilijonu(String distance) {
+	public AtstumaiTarpStoteliuPavilijonu(double distance) {
 		this.atstumasMetrais = distance;
-		naujasMetodas(distance);
+		tinkamosStoteles(atstumasMetrais);
 	}
 	
-	public String naujasMetodas(String distance) {
+	public String tinkamosStoteles(double atstumasMetrais) {
+		
+		List<Station> stoteliuSarasas = stoteles.getStoteles();
+		
+		
+		Map<String, Long> counting = stoteliuSarasas.stream().collect(
+                Collectors.groupingBy(Station::getName, Collectors.counting()));
+		
+		System.out.println(counting);
+		
 		
 		return null;
+	}
+	
+	public double atstumasTarpPavilijonu( double x1,  double y1,  double x2,  double y2 ) {
+		double distance = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+		return distance;
 	}
 }

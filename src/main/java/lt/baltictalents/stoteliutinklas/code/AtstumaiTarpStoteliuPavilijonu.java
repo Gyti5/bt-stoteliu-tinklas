a6c175b38f71f8ee88049374082bcd9a05f9b3ca
modestas612 <43955930+modestas612@users.noreply.github.com>
2018-12-10 16:15:48 +0200

    Update AtstumaiTarpStoteliuPavilijonu.java
    Padaryta 6 užduotis, bet dar neištestuota.

--- src/main/java/lt/baltictalents/stoteliutinklas/code/AtstumaiTarpStoteliuPavilijonu.java
+++ src/main/java/lt/baltictalents/stoteliutinklas/code/AtstumaiTarpStoteliuPavilijonu.java
@@ -8,11 +8,6 @@ import lt.baltictalents.stoteliutinklas.data.hardcode.HardCodedDb;
 
 public class AtstumaiTarpStoteliuPavilijonu {
 		
-	public static void main(String[] args) {
-		AtstumaiTarpStoteliuPavilijonu ms = new AtstumaiTarpStoteliuPavilijonu(30);
-		System.out.println(ms);
-	}
-	
 	private double atstumasMetrais;
 	private HardCodedDb stoteles = new HardCodedDb();
 	
@@ -22,11 +17,15 @@ public class AtstumaiTarpStoteliuPavilijonu {
 	}
 	
 	public double tinkamosStoteles(double atstumasMetrais) {
-		for ( Station a : stoteles.getStoteles()) {
+		for (Station a : stoteles.getStoteles()) {
 			List<Station> m = pavilijonaiPagalPavadinima(a.getName());
 			for (int i=0; i<m.size(); i++) {
 				for (int j=i+1; j<m.size(); j++) {
-					
+					Station station1 = m.get(i);
+					Station station2 = m.get(j);
+					if(atstumasTarpTaskuMetrais(station1, station2) <= atstumasMetrais) {
+						System.out.println(station1 + " " + station2);
+					}
 				}
 			}
 		}
@@ -41,7 +40,13 @@ public class AtstumaiTarpStoteliuPavilijonu {
 		return pavilijonaiPagalPavadinima;
 	}
 	
-	public static double atstumasTarpTaskuMetrais( double x1,  double y1,  double x2,  double y2 ) {
+	public static double atstumasTarpTaskuMetrais( Station station1, Station station2 ) {
+				
+		double x1 = Double.parseDouble(station1.getLatitude());
+		double y1 = Double.parseDouble(station1.getLongtitute());
+		double x2 = Double.parseDouble(station2.getLatitude());
+		double y2 = Double.parseDouble(station2.getLongtitute());
+		
 		final double R = 6378.1; // þemës spindulys kilometrais
 	    double latDistance = Math.toRadians(y2 - y1);
 	    double lonDistance = Math.toRadians(x2 - x1);
